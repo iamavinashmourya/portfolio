332cd2fdf2f18e499e4774dd5be741722e3f7968
Avinash Mourya <115345325+iamavinashmourya@users.noreply.github.com>
2025-12-21 13:25:48 +0530

    Enable maintenance mode in App component

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import CertificatePanel from "./components/ui/CertificatePanel";
 import ContactModal from "./components/ui/ContactModal";
 import { useState, useEffect } from "react";
 
-const MAINTENANCE_MODE = false;
+const MAINTENANCE_MODE = true;
 
 const App = () => {
   const [showCertificates, setShowCertificates] = useState(false);
